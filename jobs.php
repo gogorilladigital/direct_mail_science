6b101b18240a5a6b2b73bbf68466816a6b63ffe6
James Redd <jamesredd@Jamess-MBP.home>
2018-02-09 20:15:38 -0800

    success message

--- jobs.php
+++ jobs.php
@@ -34,7 +34,7 @@ if (
 
     $subject = 'New Job Application';
 
-    $headers = "From: " . 'hr@directmailscience.com' . "\r\n";
+    $headers = "From: " . 'cjerden@directmailscience.com' . "\r\n";
     $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
